4652a7e7cddfa3e532a773a0ba6d050fd5060299
Martin Prokopič <martin@prokopic.tk>
2018-07-31 15:35:46 +0200

    Modifications for my system

--- main.py
+++ main.py
@@ -16,12 +16,11 @@ class ElementarySessionExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        options = ['dnd', 'battery', 'lock', 'logout', 'suspend',
+        options = ['lock', 'logout', 'suspend',
                    'sleep', 'restart', 'reboot', 'shutdown', 'power-off', ]
         actions = []
         my_list = event.query.split(" ")
         if len(my_list) == 1:
-            actions.append(dnd_item())
             actions.append(lock_screen_item())
             actions.append(suspend_item())
             actions.append(reboot_item())
@@ -44,15 +43,9 @@ class KeywordQueryEventListener(EventListener):
                     elif option in ['lock']:
                         actions.append(lock_screen_item())
                         included.append('lock')
-                    elif option in ['dnd']:
-                        actions.append(dnd_item())
-                        included.append('dnd')
                     elif option in ['logout']:
                         actions.append(logout_item())
                         included.append('logout')
-                    elif option in ['battery']:
-                        actions.append(battery_percentage_item())
-                        included.append('battery')
 
             return RenderResultListAction(actions)
 
@@ -90,32 +83,6 @@ def logout_item():
                                name='Log Out',
                                description='This will close all open applications.',
                                on_enter=RunScriptAction(SessionAction.logout(), None))
-
-
-def battery_percentage_item():
-    if SessionAction.get_battery_percentage_state():
-        return ExtensionResultItem(icon='images/battery.svg',
-                                   name='Hide battery percentage.',
-                                   description='Hide battery percentage from wingpanel.',
-                                   on_enter=RunScriptAction(SessionAction.hide_battery_percentage(), None))
-    else:
-        return ExtensionResultItem(icon='images/battery.svg',
-                                   name='Show battery percentage.',
-                                   description='Show battery percentage in wingpanel.',
-                                   on_enter=RunScriptAction(SessionAction.show_battery_percentage(), None))
-
-
-def dnd_item():
-    if SessionAction.get_dnd_state():
-        return ExtensionResultItem(icon='images/system-notifications.svg',
-                                   name='Disable DND',
-                                   description='Turn off DND mode.',
-                                   on_enter=RunScriptAction(SessionAction.dnd_off(), None))
-    else:
-        return ExtensionResultItem(icon='images/system-notifications.svg',
-                                   name='Enable DND',
-                                   description='Turn on DND mode.',
-                                   on_enter=RunScriptAction(SessionAction.dnd_on(), None))
 
 
 if __name__ == '__main__':
